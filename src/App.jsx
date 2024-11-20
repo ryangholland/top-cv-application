2f5eb591b2048bdc5183b82ea97c51ba763d8d43
ryangholland <rghcode@gmail.com>
2024-11-20 08:17:47 -0500

    Install React Bootstrap and set up initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function App() {
   return (
-    <>
-      <h1>Hello world.</h1>
-    </>
+    <Container className="text-center my-4">
+      <Row>
+        <Col xs={12} lg={4} className="bg-light">Input</Col>
+        <Col xs={12} lg={8} className="bg-secondary">Result</Col>
+      </Row>
+    </Container>
   );
 }
 
